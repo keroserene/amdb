e9c6f0befda0744fffb9759a088cd6b956286630
Serene Han <keroserene@gmail.com>
2013-02-27 20:16:51 -0500

    webserver basics running now

--- amdb/views.py
+++ amdb/views.py
@@ -1,4 +1,8 @@
+
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-def index(req):
-  return render_to-response('amdb/index.html')
+def index(request):
+  return render_to_response(
+      'index.html', {}, 
+      context_instance=RequestContext(request))
